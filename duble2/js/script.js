fbdf99e89ec37b5eafa239b9de99488a22a3922c
manfbu <60040181+manfbu@users.noreply.github.com>
2020-03-30 21:52:38 +0300

    Update script.js

--- duble2/js/script.js
+++ duble2/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	$('.slider').slick({
-		prevArrow: "<img src='../img/Arow1.svg' class='prev' alt='1'>",
-    	nextArrow: "<img src='../img/Arrow2.svg' class='next' alt='2'>",
+		prevArrow: "<img src='img/Arow1.svg' class='prev' alt='1'>",
+    	nextArrow: "<img src='img/Arrow2.svg' class='next' alt='2'>",
 		infinite: true,
         slidesToShow: 2,
 		slidesToScroll: 1
