a6b9ace7dfdac4912d591ec7f25ad0f60145a1c7
manfbu <60040181+manfbu@users.noreply.github.com>
2020-06-02 21:15:26 +0300

    add the seetengs for mobile

--- Stomotolog/js/main.js
+++ Stomotolog/js/main.js
@@ -7,7 +7,18 @@ $(document).ready(function(){
     	nextArrow: "<img src='../img/next.svg' class='next' alt='2'>",
 		infinite: true,
         slidesToShow: 2,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
+                  infinite: true,
+                  dots: true
+                }
+              },
+        ]
     });
     new WOW().init();
 })
